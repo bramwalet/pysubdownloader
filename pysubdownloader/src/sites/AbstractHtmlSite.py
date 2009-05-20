6f5a8f2d3e08be8f3b6c5fac27c056233768638b
b.walet@gmail.com <b.walet@gmail.com@3ebd249c-43f7-11de-b7f0-378158e204b1>
2009-05-20 18:47:19 +0000

    - abstract RSS and HTML site to be able to implement other sites easier in the future. Only config is needed. For HTML sites there is also a required query string implementation.
    - Implemented Podnapisi

---
+++ pysubdownloader/src/sites/AbstractHtmlSite.py
@@ -0,0 +1,57 @@
+'''
+Created on 20 mei 2009
+
+@author: Bram Walet
+'''
+from sites.AbstractSubtitleSite import AbstractSubtitleSite
+from lxml.html import fromstring
+from classes.Subtitle import Subtitle
+from classes.ConfigException import ConfigException
+from handlers.FileHandler import FileHandler
+from handlers.UrlHandler import UrlHandler
+from handlers.QueryHandler import QueryHandler
+
+class AbstractHtmlSite(AbstractSubtitleSite):
+    '''
+    classdocs
+    '''
+
+       
+    def setUpHandlers(self):
+        self.fh = FileHandler()
+        self.uh = UrlHandler()
+        self.qh = QueryHandler()
+    
+    def checkConfig(self):
+        requiredKeys = ('siteName','findTableString','findDownloadLink','baseUrl','searchUrl')
+        super(AbstractHtmlSite,self).checkConfig(requiredKeys)
+    
+        
+    def createSearchQuery(self,episode): 
+        (searchKeys,sep) = self.getKeys(episode)
+        searchUrl = self.config["searchUrl"]
+        searchQueryUrl = self.qh.createSearchQuery(episode,searchUrl, searchKeys, sep)
+        return searchQueryUrl
+    
+    def search(self, episodes):
+        print "Search for each episode on: " + self.config["siteName"]
+        
+        for episode in episodes:
+            print "Search " + self.config["siteName"] + " for: " + episode.printEpisode()
+            searchUrl = self.createSearchQuery(episode)
+            sub = self.searchEpisode(episode,searchUrl)
+            if sub != None:
+                print "Match found for episode: " + episode.printEpisode + " on site " + self.siteName
+                self.downloadSubtitle(sub,episode,self.siteName)
+                
+    def searchEpisode(self,episode,searchUrl):
+        self.uh.installUrlHandler()
+        response = self.uh.executeRequest(searchUrl)
+        doc = fromstring(response.read())
+        doc.make_links_absolute(searchUrl,self.config["baseUrl"])
+        list = doc.find_class(self.config["findTableString"])
+        for listitem in list:
+            for (element, attribute, link, pos) in listitem.iterlinks():
+                if re.search(self.config["findDownloadLink"],link):
+                    return Subtitle(episode.getSerie(), episode.getSeason(), episode.getEpisode(), link)
+                    
