e2bd0280669d1749b7e0f5f9833b2e2500637b6b
b.walet <b.walet@3ebd249c-43f7-11de-b7f0-378158e204b1>
2009-05-20 04:43:00 +0000

    partial implementation of Podnapisi. Searching works, but parsing the HTML does not work because the HTML fragment is malformed.

--- pysubdownloader/src/sites/PodnapisiSite.py
+++ pysubdownloader/src/sites/PodnapisiSite.py
@@ -4,20 +4,55 @@ Created on 18 mei 2009
 @author: Bram Walet
 '''
 from sites.AbstractSubtitleSite import AbstractSubtitleSite
+from handlers.UrlHandler import UrlHandler
+from lxml.html import parse
+import StringIO
+
 
 class PodnapisiSite(AbstractSubtitleSite):
     '''
     classdocs
     '''
-    def __init__(selfparams):
+    def __init__(self):
       
-        self.uh = UrlHandler()
+       self.uh = UrlHandler()
 
     def search(self, episodes):
-        return AbstractSubtitleSite.search(self, episodes)
+        print "Search for each episode"
+        for episode in episodes:
+            episode.printEpisode()
+            self.searchEpisode(episode)
+        
+        #return AbstractSubtitleSite.search(self, episodes)
 
 
     def downloadSubtitle(self, sub, episode):
         return AbstractSubtitleSite.downloadSubtitle(self, sub, episode)
 
-        
+    
+    def searchEpisode(self,episode):
+        baseUrl = "http://simple.podnapisi.net/ppodnapisi/search?"
+        keys = ['tbsl=3',
+                'asdp=1', 
+                'sK='+ episode.getSerie(),
+                'sJ=' + '2', 
+                'sO=desc',
+                'sS=time',
+                'submit=Search', 
+                'sTS=' + episode.getSeason(),
+                'sTE=' + str(episode.getEpisode()),
+                'sY=' + episode.getYear(), 
+                'sR', 
+                'sT=1']
+        sep = '&'
+        searchUrl = baseUrl + sep.join(keys)
+        print searchUrl
+        self.uh.installUrlHandler()
+        response = self.uh.executeRequest(searchUrl)
+        print response.read()
+      
+        result = parse(searchUrl)
+        print(result)
+
+
+       
