3543674c89f5062a82ac3520855d10113373cce6
b.walet <b.walet@3ebd249c-43f7-11de-b7f0-378158e204b1>
2009-05-20 22:38:26 +0000

    refactor properties

--- pysubdownloader/src/classes/Subtitle.py
+++ pysubdownloader/src/classes/Subtitle.py
@@ -13,23 +13,66 @@ class Subtitle(object):
         self.season = season
         self.episode = episode
         self.link = link
-        
-    def  printSubtitle(self):
-        return "Subtitle for serie: " + self.serie +  " Season: " + self.season + " Episode: " + self.episode
-        
+
     def getSerie(self):
-        return self.serie
-    
+        return self._serie
+
+
     def getSeason(self):
-        return self.season
-    
+        return self._season
+
+
     def getEpisode(self):
-        return self.episode
+        return self._episode
+
 
     def getLink(self):
-        return self.link
-       
+        return self._link
+
+
+    def setSerie(self, value):
+        self._serie = value
+
+
+    def setSeason(self, value):
+        self._season = value
+
+
+    def setEpisode(self, value):
+        self._episode = value
+
+
+    def setLink(self, value):
+        self._link = value
+
+
+    def delSerie(self):
+        del self._serie
+
+
+    def delSeason(self):
+        del self._season
+
+
+    def delEpisode(self):
+        del self._episode
+
+
+    def delLink(self):
+        del self._link
+
+
+    def printSubtitle(self):
+        return "Subtitle for serie: " + self.serie +  " Season: " + self.season + " Episode: " + self.episode
+   
     def isDoubleEpisode(self):
         # TODO: this should also be implemented at Episode
         return re.match('-', self.episode)
-   
+
+    serie = property(getSerie, setSerie, delSerie, "Serie's Docstring")
+
+    season = property(getSeason, setSeason, delSeason, "Season's Docstring")
+
+    episode = property(getEpisode, setEpisode, delEpisode, "Episode's Docstring")
+
+    link = property(getLink, setLink, delLink, "Link's Docstring")
