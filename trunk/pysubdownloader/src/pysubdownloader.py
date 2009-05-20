0ef56f272e18859a3a2c9c4cc46d109dd233a0c5
b.walet <b.walet@3ebd249c-43f7-11de-b7f0-378158e204b1>
2009-05-20 14:55:21 +0000

    DEBUG: only enabled podnapisi, disabled tvsubtitles

--- trunk/pysubdownloader/src/pysubdownloader.py
+++ trunk/pysubdownloader/src/pysubdownloader.py
@@ -7,19 +7,25 @@ Created on 17 mei 2009
 import sys
 from lib.Inspector import Inspector
 from sites.TvSubtitleSite import TvSubtitleSite
+from sites.PodnapisiSite import PodnapisiSite
 
 ''' main function 
 ''' 
 if __name__ == '__main__':
     # TODO: help, proper handling
-    print("start subdownloader")
+    print("start pySubDownloader")
     path = sys.argv[1]
     inspector = Inspector()
     episodes = inspector.scan(path)
+#    try:
+#        tvsub = TvSubtitleSite()    
+#        tvsub.search(episodes)
+#    except (RuntimeError, ), e:
+#        raise
+   
     try:
-        tvsub = TvSubtitleSite()    
-        tvsub.search(episodes)
+        podnapisi = PodnapisiSite()    
+        podnapisi.search(episodes)
     except (RuntimeError, ), e:
         raise
-   
         
