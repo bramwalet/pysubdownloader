e968aa1ae4daf86d7b5b00056bb925306346687b
b.walet@gmail.com <b.walet@gmail.com@3ebd249c-43f7-11de-b7f0-378158e204b1>
2009-05-20 18:42:36 +0000

    enabled both subtitle sites

--- trunk/pysubdownloader/src/pysubdownloader.py
+++ trunk/pysubdownloader/src/pysubdownloader.py
@@ -13,17 +13,18 @@ from sites.PodnapisiSite import PodnapisiSite
 ''' 
 if __name__ == '__main__':
     # TODO: help, proper handling
-    print("start pySubDownloader")
+    print("Start pySubDownloader")
     path = sys.argv[1]
     inspector = Inspector()
     episodes = inspector.scan(path)
-#    try:
-#        tvsub = TvSubtitleSite()    
-#        tvsub.search(episodes)
-#    except (RuntimeError, ), e:
-#        raise
+    try:
+        tvsub = TvSubtitleSite()    
+        tvsub.search(episodes)
+    except (RuntimeError, ), e:
+        raise
    
     try:
+        
         podnapisi = PodnapisiSite()    
         podnapisi.search(episodes)
     except (RuntimeError, ), e:
