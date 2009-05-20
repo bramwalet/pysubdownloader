27affa0fb68e5bd483ec2ee7b898356ca39b2667
b.walet@gmail.com <b.walet@gmail.com@3ebd249c-43f7-11de-b7f0-378158e204b1>
2009-05-20 18:43:58 +0000

    - printSubtitle returns string so it can be used for logging

--- pysubdownloader/src/classes/Subtitle.py
+++ pysubdownloader/src/classes/Subtitle.py
@@ -3,7 +3,7 @@ Created on 18 mei 2009
 
 @author: Bram Walet
 '''
-
+import re
 from classes.Episode import Episode
 
 class Subtitle(object):
@@ -16,7 +16,7 @@ class Subtitle(object):
         self.link = link
         
     def  printSubtitle(self):
-        print "Subtitle for serie: " + self.serie +  " Season: " + self.season + " Episode: " + self.episode
+        return "Subtitle for serie: " + self.serie +  " Season: " + self.season + " Episode: " + self.episode
         
     def getSerie(self):
         return self.serie
@@ -29,14 +29,7 @@ class Subtitle(object):
 
     def getLink(self):
         return self.link
-    
-    def getId(self):
-        # TODO: this should be in a parser, and should be fixed better
-        rightIdx = self.link.rfind('.')
-        leftIdx = self.link.find('-')
-        id = self.link[leftIdx+1:rightIdx]
-        return id
-    
+       
     def isDoubleEpisode(self):
         # TODO: this should also be implemented at Episode
         return re.match('-', self.episode)
